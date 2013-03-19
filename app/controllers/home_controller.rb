3c34e9a64832ddfed93a77c22588d82871fb57b4
alexoviedo999 <alexoviedo999@gmail.com>
2013-03-19 01:51:45 -0400

    Added photo editing to home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   layout 'homepage'
+
   def index
+    @movies = Movie.all
+  end
+
+  def photomini
+    matrix = ImageList.new("http://ia.media-imdb.com/images/M/MV5BMTkzMTUwMDAyMl5BMl5BanBnXkFtZTcwMDIwMTQ1OA@@._V1_SX300.jpg")
+    smallmatrix = matrix.minify
+    smallmatrix.display
   end
 
 end
